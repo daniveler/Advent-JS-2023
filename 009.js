3d41734302a94456e0851a5b4fd64241f29d9dd2
daniveler <daniel01velerdas@gmail.com>
2023-12-13 12:35:16 +0100

    Started 009

---
+++ 009.js
@@ -0,0 +1,16 @@
+function adjustLights(lights) {
+  // Code here
+  return 0
+}
+
+adjustLights(['🟢', '🔴', '🟢', '🟢', '🟢'])
+// -> 1 (cambias la cuarta luz a 🔴)
+
+adjustLights(['🔴', '🔴', '🟢', '🟢', '🔴'])
+// -> 2 (cambias la segunda luz a 🟢 y la tercera a 🔴)
+
+adjustLights(['🟢', '🔴', '🟢', '🔴', '🟢'])
+// -> 0 (ya están alternadas)
+
+adjustLights(['🔴', '🔴', '🔴'])
+// -> 1 (cambias la segunda luz a 🟢)
